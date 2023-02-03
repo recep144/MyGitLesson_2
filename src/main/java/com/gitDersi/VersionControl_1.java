be39d6b49d75c48b5187c44b82dd2f830aa38e3a
recep <recepacar1979@gmail.com>
2023-02-03 11:56:23 +0300

    added first code

--- src/main/java/com/gitDersi/VersionControl_1.java
+++ src/main/java/com/gitDersi/VersionControl_1.java
@@ -3,7 +3,7 @@ package com.gitDersi;
 public class VersionControl_1 {
 
     public static void main(String[] args) {
-        System.out.println("first code");
+        System.out.println("first new code");
 
     }
 
