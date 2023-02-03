c7e7972cb926c2744b02dc6932e49c18743bc9e7
recep144 <119009613+recep144@users.noreply.github.com>
2023-02-03 12:09:09 +0300

    second message writed

--- src/main/java/com/gitDersi/VersionControl_1.java
+++ src/main/java/com/gitDersi/VersionControl_1.java
@@ -5,7 +5,7 @@ public class VersionControl_1 {
     public static void main(String[] args) {
         System.out.println("first new code");
         //gitHubdan eklenmistir
-        
+        System.out.println("gitHubdan yazıldı")
     }
 
 }
